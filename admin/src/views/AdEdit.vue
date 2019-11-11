6bd106d686825b0ce16d4c29463f5edba1f2fff7
Neilyoz <neilyoz@foxmail.com>
2019-11-11 20:30:24 +0800

    format code

--- admin/src/views/AdEdit.vue
+++ admin/src/views/AdEdit.vue
@@ -22,16 +22,15 @@
                 :action="uploadUrl"
                 :show-file-list="false"
                 :headers="getAuthHeaders()"
-                :on-success="res => $set(item, 'image', res.url)">
-                <img v-if="item.image" :src="item.image" class="avatar">
+                :on-success="res => $set(item, 'image', res.url)"
+              >
+                <img v-if="item.image" :src="item.image" class="avatar" />
                 <i v-else class="el-icon-plus avatar-uploader-icon"></i>
               </el-upload>
             </el-form-item>
 
             <el-form-item>
-              <el-button size="small" type="danger" @click="model.items.splice(index, 1)">
-                删除
-              </el-button>
+              <el-button size="small" type="danger" @click="model.items.splice(index, 1)">删除</el-button>
             </el-form-item>
           </el-col>
         </el-row>
@@ -45,47 +44,47 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      id: {}
-    },
+export default {
+  props: {
+    id: {}
+  },
 
-    data () {
-      return {
-        model: {
-          items: []
-        },
+  data() {
+    return {
+      model: {
+        items: []
+      }
+    }
+  },
+
+  created() {
+    this.id && this.fetch()
+  },
+
+  methods: {
+    async save() {
+      let res
+
+      if (this.id) {
+        res = await this.$http.put(`/rest/ads/${this.id}`, this.model)
+      } else {
+        res = await this.$http.post('/rest/ads', this.model)
+      }
+
+      if (res.status === 200) {
+        await this.$router.push('/ads/list')
+
+        this.$message({
+          type: 'success',
+          message: '保存成功'
+        })
       }
     },
 
-    created () {
-      this.id && this.fetch()
-    },
-
-    methods: {
-      async save () {
-        let res
-
-        if (this.id) {
-          res = await this.$http.put(`/rest/ads/${this.id}`, this.model)
-        } else {
-          res = await this.$http.post('/rest/ads', this.model)
-        }
-
-        if (res.status === 200) {
-          await this.$router.push('/ads/list')
-
-          this.$message({
-            type: 'success',
-            message: '保存成功'
-          })
-        }
-      },
-
-      async fetch () {
-        const res = await this.$http.get(`/rest/ads/${this.id}`)
-        this.model = Object.assign({}, this.model, res.data)
-      },
+    async fetch() {
+      const res = await this.$http.get(`/rest/ads/${this.id}`)
+      this.model = Object.assign({}, this.model, res.data)
     }
   }
+}
 </script>
--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -24,10 +24,9 @@
       </div>
     </div>
 
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style scoped>
-
 </style>
